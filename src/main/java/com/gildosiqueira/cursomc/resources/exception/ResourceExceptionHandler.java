ca93e6b0b9fb8af97d0427106e649031cfaaa11e
Gildo Siqueira <gildotsiqueira@gmail.com>
2019-09-12 17:37:54 -0300

    Deletando uma categoria com DELETE

--- src/main/java/com/gildosiqueira/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/gildosiqueira/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.gildosiqueira.cursomc.services.exception.DataIntegrityException;
 import com.gildosiqueira.cursomc.services.exception.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -16,7 +17,13 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> objectNotFount(ObjectNotFoundException e, HttpServletRequest request){
 
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
-
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err); 
+	}
+	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> DataIntegrit(DataIntegrityException e, HttpServletRequest request){
+
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err); 
 	}
 }
