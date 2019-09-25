3c7b098e8edba359601c2555877a2df0fb3ee6e8
Gildo Siqueira <gildotsiqueira@gmail.com>
2019-09-25 17:41:16 -0300

    Salvando perfil de usuario na base de dados

--- src/main/java/com/gildosiqueira/cursomc/services/DBService.java
+++ src/main/java/com/gildosiqueira/cursomc/services/DBService.java
@@ -20,6 +20,7 @@ import com.gildosiqueira.cursomc.domain.PagamentoComCartao;
 import com.gildosiqueira.cursomc.domain.Pedido;
 import com.gildosiqueira.cursomc.domain.Produto;
 import com.gildosiqueira.cursomc.domain.enums.EstadoPagamento;
+import com.gildosiqueira.cursomc.domain.enums.Perfil;
 import com.gildosiqueira.cursomc.domain.enums.TipoCliente;
 import com.gildosiqueira.cursomc.repositories.CategoriaRepository;
 import com.gildosiqueira.cursomc.repositories.CidadeRepository;
@@ -116,13 +117,19 @@ public class DBService {
 		Cliente cli1 = new Cliente(null, "Maria SIlva", "gildo_siqueira@hotmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
 		cli1.getTelefons().addAll(Arrays.asList("27363323", "93838393"));
 
+		Cliente cli2 = new Cliente(null, "Ana Costa", "gildotsiqueira@gmail.com", "31628382748", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		cli2.getTelefons().addAll(Arrays.asList("12343323", "94321393"));
+		cli2.addPerfil(Perfil.ADMIN);
+		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		Endereco e3 = new Endereco(null, "Avenida Flores", "2105", null, "Centro", "54377012", cli1, c2);
 
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli1.getEnderecos().addAll(Arrays.asList(e3));
 
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy HH:mm");
 		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
