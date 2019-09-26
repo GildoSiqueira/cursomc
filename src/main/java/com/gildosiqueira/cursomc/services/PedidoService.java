65d35779b798d9d3e10852cbe263d313f2ebee9b
Gildo Siqueira <gildotsiqueira@gmail.com>
2019-09-26 15:53:57 -0300

    Refresh token

--- src/main/java/com/gildosiqueira/cursomc/services/PedidoService.java
+++ src/main/java/com/gildosiqueira/cursomc/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.gildosiqueira.cursomc.domain.Cliente;
 import com.gildosiqueira.cursomc.domain.ItemPedido;
 import com.gildosiqueira.cursomc.domain.PagamentoComBoleto;
 import com.gildosiqueira.cursomc.domain.Pedido;
@@ -14,6 +18,8 @@ import com.gildosiqueira.cursomc.domain.enums.EstadoPagamento;
 import com.gildosiqueira.cursomc.repositories.ItemPedidoRepository;
 import com.gildosiqueira.cursomc.repositories.PagamentoRepository;
 import com.gildosiqueira.cursomc.repositories.PedidoRepository;
+import com.gildosiqueira.cursomc.security.UserSS;
+import com.gildosiqueira.cursomc.services.exception.AuthorizationException;
 import com.gildosiqueira.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
@@ -70,5 +76,15 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {			
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado!");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);		
 	}
 }
