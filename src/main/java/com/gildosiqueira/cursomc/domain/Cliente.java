19fbd95194b50c5c72fb76c5ffa20bc5ec18e8ec
Gildo Siqueira <gildotsiqueira@gmail.com>
2019-10-02 21:27:14 -0300

    Usando padrão de nomes para imagens

--- src/main/java/com/gildosiqueira/cursomc/domain/Cliente.java
+++ src/main/java/com/gildosiqueira/cursomc/domain/Cliente.java
@@ -55,8 +55,6 @@ public class Cliente implements Serializable{
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
-	private String ImageUrl;
-	
 	public Cliente() {
 		addPerfil(Perfil.CLIENTE);
 	}
@@ -175,14 +173,6 @@ public class Cliente implements Serializable{
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}
-
-	public String getImageUrl() {
-		return ImageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		ImageUrl = imageUrl;
 	}
 
 }
--- src/main/java/com/gildosiqueira/cursomc/services/ClienteService.java
+++ src/main/java/com/gildosiqueira/cursomc/services/ClienteService.java
@@ -1,10 +1,12 @@
 package com.gildosiqueira.cursomc.services;
 
+import java.awt.image.BufferedImage;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -39,9 +41,15 @@ public class ClienteService {
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
-	
+
 	@Autowired
 	private S3Service s3Service;
+	
+	@Autowired
+	private ImageService imageService;
+	
+	@Value("${img.prefix.client.profile}")
+	private String prefix;
 	
 	public Cliente find(Integer id) {
 		
@@ -115,19 +123,15 @@ public class ClienteService {
 	
 	public URI uploadProfilePicture(MultipartFile multipartFile) {
 		
-		UserSS user = UserService.authenticated();
-		
+		UserSS user = UserService.authenticated();	
 		if (user == null) {
 			throw new AuthorizationException("Avesso negado!");
 		}
 		
-		URI uri = s3Service.uploadFile(multipartFile);
-
-		Cliente cli = find(user.getId());
-		cli.setImageUrl(uri.toString());
-		repo.save(cli);
-
-		return uri;	
+		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		String fileName = prefix + user.getId() + ".jpg";
 		
+		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
+			
 	}
 }
